1909f18d3c45b0f5e433a9a3e343679616b4dddf
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-09-29 23:41:24 -0300

    projeto login 01

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+    session_start();
+    if($_SESSION['usuario']){
+        header('Location: login.php');
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,6 +21,9 @@
         <h1>Curso PHP</h1>
         <h2>Índice dos Exercícios</h2>
     </header>
+    <nav class="navegacao">
+       
+    </nav>
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Seja bem vindo</h2>
+    </header>
+    <nav class="navegacao">
+       
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+                
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS © <?= date('Y'); ?>
+    </footer>
+</body>
+
+</html>
