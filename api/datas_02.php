6fb2c385e5dc7c5e4d81b33ea3f680814009f40f
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-07 20:26:03 -0300

    datas 02

---
+++ api/datas_02.php
@@ -0,0 +1,47 @@
+<div class="titulo">Datas 02</div>
+
+<?php
+
+$formatoData1 = 'D,d \d\e M \d\e Y';
+$formatoData2 = '%A,%d de %B de %Y';
+$formatoDataHora = '%A, %de de %B de %Y - %H:%M:%S';
+
+$agora = new DateTime();
+
+print_r($agora);
+echo '<br>';
+
+echo $agora->format($formatoData1) . '<br>';
+
+setlocale(LC_TIME,'pt-BR');
+echo strftime($formatoData2,$agora->getTimestamp()) . '<br>';
+
+
+$dataFixa = new DateTime('1975-01-25 15:30:50');
+
+echo strftime($formatoDataHora,$dataFixa->getTimestamp()) . '<br>';
+
+$amanha = new DateTime('+1 day');
+echo strftime($formatoData2,$amanha->getTimestamp()) . '<br>';
+
+$amanha->modify('+1 week');
+echo strftime($formatoDataHora,$amanha->getTimestamp()) . '<br>';
+
+$amanha->setDate(2000,5,20);
+echo strftime($formatoDataHora,$dataFixa->getTimestamp()) . '<br>';
+
+$dataPassada = new DateTime('2000-05-17');
+$dataFutura = new DateTime('2030-11-26');
+$outraData = new DateTime('2030-11-26');
+
+echo ($amanha > $dataPassada ? 'Maior' : 'Menor') . '<br>';
+echo ($amanha > $dataFutura ? 'Maior' : 'Menor') . '<br>';
+echo ($outraData == $dataFutura ? 'Iguais' : 'Diferentes') . '<br>';
+
+
+$tz = new DateTimeZone('America/Sao_Paulo');
+$agora = new DateTime(null,$tz);
+echo $agora->format('d/M/Y H:i:s');
+
+
+
--- menu.php
+++ menu.php
@@ -434,6 +434,10 @@
                                 Datas 01
                             </a>
                         </li>
+                        <li><a href="exercicio.php?dir=api&file=datas_02">
+                                Datas 02
+                            </a>
+                        </li>
                         
                     </ul>
                 </div>
