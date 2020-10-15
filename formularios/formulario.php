ea730c48da061967c186c4e28243a6fb1e88b5da
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-15 20:48:52 -0300

    criando filtros de validação para os inputs do formulário

--- formularios/formulario.php
+++ formularios/formulario.php
@@ -5,8 +5,47 @@
 <h2>Cadastro</h2>
 
 <?php
+if(count($_POST) > 0){
+    //validação do nome
+    if(!filter_input(INPUT_POST,"nome")){
+        echo "Nome é obrigatório" , '<br>';
+    }
+    //validação de data
+    if(!filter_input(INPUT_POST,"nascimento")){
+        $data = DateTime::createFromFormat('d/m/Y',$_POST['nascimento']);
+        if(!$data){
+            echo "A data deve estar no formato padrão dd/mm/aaaa", '<br>';
+        }
+    }
+    //validação de email
+    if(!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+        echo "Email inválido", '<br>';
+    }
 
+    //validação de site
+    if(!filter_var($_POST['site'],FILTER_VALIDATE_URL)){
+        echo "Site inválido", '<br>';
+    }
+
+    $filhosConfig = [
+        "options" => ["min_range"=>0, "max_range"=>20]
+    ];
+    
+    //validação de filhos
+    if(!filter_var($_POST['filhos'],FILTER_VALIDATE_INT, $filhosConfig) && $_POST['filhos'] != 0 ){
+        echo "Quantidade de filhos inválida";
+    }
+
+    $salarioConfig = ['options'=>['decimal'=>',']];
+
+    //validação de salário
+    if(!filter_var($_POST['salario'],FILTER_VALIDATE_FLOAT,$salarioConfig)){
+        echo "Salário em formato inválido";
+    }
+}
 ?>
+
+
 
 <form action="#" method="post">
     <div class="form-row">
