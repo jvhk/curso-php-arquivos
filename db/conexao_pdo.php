214a63f90c587f6d9d1afafc4380a0a191346b8d
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-10-24 23:41:05 -0300

    conexao com PDO

---
+++ db/conexao_pdo.php
@@ -0,0 +1,18 @@
+<?php
+
+function novaConexao($banco = 'primeiro_banco'){
+    $servidor = '127.0.0.1';
+    $usuario = 'root';
+    $senha = 'root';
+
+
+    try{
+        $conexao = new PDO("mysql:host=$servidor;dbname=$banco", $usuario, $senha);
+        return $conexao;
+    }catch(PDOException $e){
+        die('Erro: '. $e->getMessage());
+    }
+}
+
+novaConexao();
+echo "Fim";
