678231aa2c8160495f9e147c13a37927508e9fc4
jvhk <joaovitoroliveiracamara@hotmail.com>
2020-11-29 20:17:43 -0300

    modificações no menu lateral

--- innout/src/views/template/header.php
+++ innout/src/views/template/header.php
@@ -34,7 +34,7 @@
                 <ul class="nav-list">
                     <li class="nav-item">
                         <a href="logout.php">
-                            <i class="iconfont-logout mr-2"></i>
+                            <i class="icofont-logout mr-2"></i>
                             Sair
                         </a>
                     </li>
